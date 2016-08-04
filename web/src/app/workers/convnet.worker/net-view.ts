8caadf1cc0d0a5e30e96c38d858ce31ee9c37895
Aliaksandr Karpovich <aliaksandr.karpovich@itechart-group.com>
2016-08-04 18:15:58 +0300

    working changes

--- web/src/app/workers/convnet.worker/net-view.ts
+++ web/src/app/workers/convnet.worker/net-view.ts
@@ -10,14 +10,15 @@ export class NetView {
     private dist: ImageData;
 
     constructor(private data: any) {
-        this.dist = NetView.createImageData(this.width, this.height);
+        this.dist = new ImageData(this.width, this.height);
     }
 
-    static putImageData(destImageData, srcImageData, dx, dy, dirtyX = undefined, dirtyY = undefined, dirtyWidth = undefined, dirtyHeight = undefined) {
+    static putImageData(distImageData, srcImageData, dx, dy,
+                        dirtyX = undefined, dirtyY = undefined, dirtyWidth = undefined, dirtyHeight = undefined) {
         let data = srcImageData.data;
         let height = srcImageData.height;
         let width = srcImageData.width;
-        let destWidth = destImageData.width;
+        let distWidth = distImageData.width;
 
         dirtyX = dirtyX || 0;
         dirtyY = dirtyY || 0;
@@ -30,32 +31,32 @@ export class NetView {
         for (let y = dirtyY; y < limitBottom; y++) {
             for (let x = dirtyX; x < limitRight; x++) {
                 let pos = y * width + x;
-                let destPos = (y + dy) * destWidth + (x + dx);
+                let distPos = (y + dy) * distWidth + (x + dx);
 
-                destImageData.data[destPos * 4] = data[pos * 4];
-                destImageData.data[destPos * 4 + 1] = data[pos * 4 + 1];
-                destImageData.data[destPos * 4 + 2] = data[pos * 4 + 2];
-                destImageData.data[destPos * 4 + 3] = data[pos * 4 + 3];
+                distImageData.data[distPos * 4] = data[pos * 4];
+                distImageData.data[distPos * 4 + 1] = data[pos * 4 + 1];
+                distImageData.data[distPos * 4 + 2] = data[pos * 4 + 2];
+                distImageData.data[distPos * 4 + 3] = data[pos * 4 + 3];
             }
         }
     }
 
-    static createImageData(width, height): ImageData {
-        return new ImageData(width, height);
-    }
-
     static fillRect(dist, x, y, width, height, color) {
         let offset = 4 * (x + dist.width * y);
+
         for (let j = 0; j < height; j++) {
             for (let i = 0; i < width; i++) {
+
                 let idx = 4 * (i + dist.width * j) + offset;
                 dist.data[idx] = dist.data[idx + 1] = dist.data[idx + 2] = color;
+                dist.data[idx + 3] = 255;
+
             }
         }
     }
 
     prepareImageData() {
-        let inImgData = NetView.createImageData(this.data.size[0], this.data.size[1]);
+        let inImgData = new ImageData(this.data.size[0], this.data.size[1]);
 
         for (let i = 0; i < this.data.in.length; i++) {
             let j = i * 4;
@@ -83,7 +84,7 @@ export class NetView {
         let startx = (this.width - bw) / 2;
         let step = layer.outSize[0] + HSTEP;
         for (let k = 0; k < layer.inCount * layer.count; k++) {
-            let imData = NetView.createImageData(layer.outSize[0], layer.outSize[1]);
+            let imData = new ImageData(layer.outSize[0], layer.outSize[1]);
             let offset = k * layer.outSize[0] * layer.outSize[1];
 
             for (let i = 0; i < layer.out.length; i++) {
@@ -103,7 +104,7 @@ export class NetView {
         let startx = (this.width - bw) / 2;
         let step = layer.outSize[0] + HSTEP;
         for (let k = 0; k < layer.count; k++) {
-            let imData = NetView.createImageData(layer.outSize[0], layer.outSize[1]);
+            let imData = new ImageData(layer.outSize[0], layer.outSize[1]);
             let offset = k * layer.outSize[0] * layer.outSize[1];
 
             for (let i = 0; i < layer.out.length; i++) {
@@ -121,18 +122,17 @@ export class NetView {
         let lineCount = Math.floor(this.width / (FC_SIZE + 1));
         let preSize = layer.out.length * (FC_SIZE + 1) - 1;
         let blockHeight = Math.ceil(preSize / lineCount);
-        let xoffset = lineCount < layer.out.length ? 0 : (this.width - preSize) / 2;
+        let xoffset = lineCount < layer.out.length ? 0 : Math.ceil((this.width - preSize) / 2);
 
-        let j = 0, k = 0;
-        for (let i = 0; i < layer.out.length; i++, j++) {
+        for (let i = 0, j = 0, k = 0; i < layer.out.length; i++, j++) {
             if (j >= lineCount) {
                 j = 0;
                 k++;
             }
             let x = j * (FC_SIZE + 1) + xoffset,
-                c = Math.floor(layer.out[i] * 255);
+                color = Math.floor(layer.out[i] * 255);
 
-            NetView.fillRect(this.dist, x, this.vpos + k * (FC_SIZE + 1), FC_SIZE, FC_SIZE, c);
+            NetView.fillRect(this.dist, x, this.vpos + k * (FC_SIZE + 1), FC_SIZE, FC_SIZE, color);
         }
 
         this.vpos += blockHeight + VSTEP;
@@ -141,18 +141,17 @@ export class NetView {
     processOutputLayer(layer: any) {
         let lineCount = Math.floor(this.width / (OUT_SIZE + 1));
         let blockHeight = Math.ceil(layer.out.length * OUT_SIZE / lineCount);
-        let xoffset = lineCount < layer.out.length ? 0 : (this.width - layer.out.length * (OUT_SIZE + 1) - 1) / 2;
+        let xoffset = lineCount < layer.out.length ? 0 : Math.ceil((this.width - layer.out.length * (OUT_SIZE + 1) - 1) / 2);
 
-        let j = 0, k = 0;
-        for (let i = 0; i < layer.out.length; i++, j++) {
+        for (let i = 0, j = 0, k = 0; i < layer.out.length; i++, j++) {
             if (j >= lineCount) {
                 j = 0;
                 k++;
             }
             let x = j * (OUT_SIZE + 1) + xoffset,
-                c = Math.floor(layer.out[i] * 255);
+                color = Math.floor(layer.out[i] * 255);
 
-            NetView.fillRect(this.dist, x, this.vpos + k * (OUT_SIZE + 1), OUT_SIZE, OUT_SIZE, c);
+            NetView.fillRect(this.dist, x, this.vpos + k * (OUT_SIZE + 1), OUT_SIZE, OUT_SIZE, color);
         }
 
         this.vpos += blockHeight + VSTEP;
